93159b4dca47b46067e56bbd8d2ca5fa009a06a2
Daniel Leib <daniel.leib@gmail.com>
2019-08-09 18:59:19 -0500

    got tracking working

--- src/main/java/com/movsci/processingapi/helpers/VideoHelpers.java
+++ src/main/java/com/movsci/processingapi/helpers/VideoHelpers.java
@@ -2,18 +2,20 @@ package com.movsci.processingapi.helpers;
 
 import com.movsci.processingapi.Model.MovSciPoint;
 import lombok.extern.slf4j.Slf4j;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.VideoWriter;
 import org.opencv.videoio.Videoio;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.opencv.core.TermCriteria.COUNT;
 
 @Slf4j
 public class VideoHelpers {
@@ -24,8 +26,8 @@ public class VideoHelpers {
         double fps = cap.get(Videoio.CAP_PROP_FPS);
 
         //initialize stuff needed for tracking
+        ArrayList<ArrayList<MovSciPoint>> points = new ArrayList<>();
         ArrayList<MovSciPoint> initialPoints = new ArrayList<>();
-        ArrayList<MovSciPoint> trailingPoints = new ArrayList<>();
         ArrayList<MovSciPoint> nextPoints = new ArrayList<>();
 
         //parse input pointDefs - iterate through by number of inputs per point
@@ -34,12 +36,12 @@ public class VideoHelpers {
         for (int i=0; i<str.size(); i=i+7) {
             MovSciPoint pt = new MovSciPoint();
             pt.setType(str.get(i));
-            pt.setColor(new Scalar(Integer.valueOf(str.get(i+1)),Integer.valueOf(str.get(i+2)),Integer.valueOf(str.get(i+3)),0));
-            pt.setRadius(Integer.valueOf(str.get(i+4)));
-            pt.setPoint(new Point(Double.valueOf(str.get(i+5)),Double.valueOf(str.get(i+6))));
+            pt.setColor(new Scalar(Integer.parseInt(str.get(i+1)),Integer.parseInt(str.get(i+2)),Integer.parseInt(str.get(i+3)),0));
+            pt.setRadius(Integer.parseInt(str.get(i+4)));
+            pt.setPoint(new Point(Double.parseDouble(str.get(i+5)),Double.parseDouble(str.get(i+6))));
             initialPoints.add(pt);
         }
-        int numPoints = initialPoints.size();
+        int numPoints = points.size();
 
         //set output video
 
@@ -48,6 +50,7 @@ public class VideoHelpers {
         VideoWriter videoWriter = new VideoWriter(outPath + "\\" + outName,VideoWriter.fourcc('x','2','6','4'),fps,frameSize,true);
         //read frames and track
         Mat nextMat = new Mat();
+        Mat prevMat = new Mat();
         int frame = 0;
         while(cap.read(nextMat)){
             if(frame == 0){
@@ -58,15 +61,21 @@ public class VideoHelpers {
                     e.printStackTrace();
                 }
                 Write(nextMat, videoWriter);
+                prevMat = nextMat.clone();
+                frame++;
             }else{
                 //track points, initial to next
                 try {
-                    FrameHelpers.DrawOnFrame(nextMat,initialPoints);
-                    //FrameHelpers.DrawOnFrame(nextMat,nextPoints);
+                    nextPoints = advanceTrackedFrame(prevMat,nextMat,initialPoints);
+                    points.add(nextPoints);
+                    initialPoints.clear();
+                    initialPoints.addAll(nextPoints);
+                    FrameHelpers.DrawOnFrame(nextMat,nextPoints);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 Write(nextMat,videoWriter);
+                prevMat = nextMat.clone();
             }
         }
         videoWriter.release();
@@ -74,6 +83,45 @@ public class VideoHelpers {
         return true;
     }
 
+    private static ArrayList<MovSciPoint> advanceTrackedFrame(Mat prevMat, Mat nextMat, ArrayList<MovSciPoint> currPoints){
+        //initialize required objects
+        Mat nextGray = new Mat(prevMat.rows(), prevMat.cols(), CvType.CV_8UC3);
+        Mat prevGray = new Mat(prevMat.rows(), prevMat.cols(), CvType.CV_8UC3);
+        MatOfPoint2f prevFeatures = new MatOfPoint2f();
+        MatOfPoint2f nextFeatures = new MatOfPoint2f();
+        MatOfByte status = new MatOfByte();
+        MatOfFloat err = new MatOfFloat();
+        Size winSize = new Size(20, 20);//can be customized
+        double epsilon = 0.001;
+        TermCriteria termCrit = new TermCriteria(COUNT, 20, epsilon);
+        int maxLevel = 3;
+        double eigenThreshold = 0.001;
+        Imgproc.cvtColor(nextMat,nextGray, Imgproc.COLOR_RGBA2GRAY);
+        Imgproc.cvtColor(prevMat,prevGray, Imgproc.COLOR_RGBA2GRAY);
+        ArrayList<Point> points = new ArrayList<>();
+        List<Point> nextPointsP = new ArrayList<>();
+        ArrayList<MovSciPoint> nextPointsM = new ArrayList<>();
+        for(MovSciPoint p : currPoints){
+            points.add(p.getPoint());
+        }
+        prevFeatures.fromList(points);
+        nextFeatures.fromArray(prevFeatures.toArray());//helps initial estimate
+
+        Video.calcOpticalFlowPyrLK(prevGray, nextGray, prevFeatures, nextFeatures, status, err, winSize, maxLevel, termCrit, 0, eigenThreshold);
+        nextPointsP = nextFeatures.toList();
+        for(int i=0; i < currPoints.size(); i++){
+            MovSciPoint pt = new MovSciPoint(currPoints.get(i).getType()
+                    ,currPoints.get(i).getTrailingPoints()
+                    ,currPoints.get(i).getColor()
+                    ,currPoints.get(i).getThickness()
+                    ,currPoints.get(i).getLineType()
+                    ,currPoints.get(i).getRadius()
+                    ,nextPointsP.get(i));
+            nextPointsM.add(pt);
+        }
+
+        return nextPointsM;//change to movscipoints
+    }
 
 
     private static void Write(Mat frame, VideoWriter videoWriter) {
